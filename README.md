53011fcfe8473bec9551b285f426b0611c803a24
Bishtawi <bishtawi@users.noreply.github.com>
2022-05-28 16:28:56 -0700

    Initial Rust setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # raytracer
+
+Following guide from https://raytracing.github.io/ as a way to practice Rust
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+#![forbid(unsafe_code)]
+#![deny(clippy::all, clippy::pedantic)]
+
+fn main() {
+    println!("Hello, world!");
+}
